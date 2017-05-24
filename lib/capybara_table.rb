80e46050b34c2b8fefbb3f6a899e4fc70c723d22
Jonas Nicklas <jonas.nicklas@gmail.com>
2017-05-24 22:50:03 +0200

    More extractions

--- lib/capybara_table.rb
+++ lib/capybara_table.rb
@@ -2,27 +2,36 @@ require "capybara_table/version"
 require "capybara"
 
 module CapybaraTable
-  extend self
+  module Expressions
+    extend self
 
-  def cell_position(node)
-    without_colspan = node.axis(:"preceding-sibling")[XPath.attr(:colspan).inverse].count
-    with_colspan = node.axis(:"preceding-sibling").attr(:colspan).method(:sum)
-    exists = node.method(:boolean).method(:number)
+    def table_cell(header, value)
+      XPath.descendant(:td, :th)[XPath.string.n.is(value).and(header_position(header).equals(self_position))]
+    end
 
-    without_colspan.plus(with_colspan).plus(exists)
+    def self_position
+      cell_position(XPath.axis(:self))
+    end
+
+    def header_position(header)
+      header_node = XPath.axis(:ancestor, :table)[1].descendant(:tr)[1].descendant(:th)[XPath.string.n.is(header)]
+      cell_position(header_node)
+    end
+
+    def cell_position(node)
+      without_colspan = node.axis(:"preceding-sibling")[XPath.attr(:colspan).inverse].count
+      with_colspan = node.axis(:"preceding-sibling").attr(:colspan).method(:sum)
+      exists = node.method(:boolean).method(:number)
+
+      without_colspan.plus(with_colspan).plus(exists)
+    end
   end
 end
 
 Capybara.add_selector :table_row do
   xpath do |fields|
     fields.reduce(XPath.descendant(:tr)) do |xpath, (header, value)|
-      header_node = XPath.axis(:ancestor, :table)[1].descendant(:tr)[1].descendant(:th)[XPath.string.n.is(header)]
-      header_position = CapybaraTable.cell_position(header_node)
-
-      cell_node = XPath.axis(:self)
-      cell_position = CapybaraTable.cell_position(cell_node)
-
-      xpath[XPath.descendant(:td, :th)[XPath.string.n.is(value).and(header_position.equals(cell_position))]]
+      xpath[CapybaraTable::Expressions.table_cell(header, value)]
     end
   end
 end
